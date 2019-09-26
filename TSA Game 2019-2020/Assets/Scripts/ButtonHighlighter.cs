51be300345ea9187e14d27072e2cadb83d481920
Gabriel Moncau <gabriel.moncau@hotmail.com>
2019-09-25 20:10:31 -0400

    Water tweaks, UI animation testing

---
+++ TSA Game 2019-2020/Assets/Scripts/ButtonHighlighter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHighlighter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponent<Animation>().Play("ButtonHighlight");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponent<Animation>().Play("ButtonUnHighlight");
+    } 
+}
