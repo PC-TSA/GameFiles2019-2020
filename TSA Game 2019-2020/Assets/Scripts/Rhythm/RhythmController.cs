cf02748ba90ce7b8e00640aa2eee89d29452f0e9
Gabriel Moncau <gabriel.moncau@hotmail.com>
2019-12-13 11:10:25 -0500

    Reverted to Azure Storage setup
    Abandoning SQL indefinitively due to time constraints and technical difficulties

--- TSA Game 2019-2020/Assets/Scripts/Rhythm/RhythmController.cs
+++ TSA Game 2019-2020/Assets/Scripts/Rhythm/RhythmController.cs
@@ -67,6 +67,8 @@ public class RhythmController : MonoBehaviour
 
     public GameObject splashTitlePrefab;
 
+    public WorkshopController workshopController;
+
     private void Start()
     {
         Object[] temp = Resources.LoadAll("Songs", typeof(AudioClip)); //Read all audioclips in the Resources/Songs folder and add them to the 'Songs' list
@@ -597,6 +599,6 @@ public class RhythmController : MonoBehaviour
 
     public void UploadRecording()
     {
-        //workshopController.UploadRecording("gabrieltm9", savedRecordingPath, savedRecordingName + ".xml");
+        workshopController.UploadRecording("gabrieltm9", savedRecordingPath, savedRecordingName + ".xml");
     }
 }
