328907ee1f5c76849231477c8186fcd2812e51e3
Gabriel Moncau <gabriel.moncau@hotmail.com>
2020-02-05 11:25:40 -0500

    Prefab Replacer, tweaks

---
+++ TSA Game 2019-2020/Assets/Scripts/PrefabReplacer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabReplacer : MonoBehaviour
+{
+    public Transform parentOfReplacees;
+    public Transform newParent;
+    public GameObject prefabReplacer;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            Replace();
+    }
+
+    void Replace()
+    {
+        for (int i = 0; i < parentOfReplacees.childCount; i++)
+        {
+            Transform temp = parentOfReplacees.GetChild(i);
+            GameObject newObj = Instantiate(prefabReplacer, temp.position, temp.rotation, newParent);
+            newObj.transform.localScale = temp.localScale;
+        }
+        Destroy(parentOfReplacees.gameObject);
+    }
+}
