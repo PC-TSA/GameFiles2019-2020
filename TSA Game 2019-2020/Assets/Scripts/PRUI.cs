50070943e176b2acdc33faa609b8faa825b34a1d
Gabriel Moncau <gabriel.moncau@hotmail.com>
2020-03-04 21:18:31 -0500

    HDRP work

---
+++ TSA Game 2019-2020/Assets/Scripts/PRUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PrefabReplacer))]
+public class PRUI : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PrefabReplacer myScript = (PrefabReplacer)target;
+        if (GUILayout.Button("Replace"))
+        {
+            myScript.Replace();
+        }
+    }
+}
--- TSA Game 2019-2020/Assets/Scripts/PrefabReplacer.cs
+++ TSA Game 2019-2020/Assets/Scripts/PrefabReplacer.cs
@@ -8,20 +8,33 @@ public class PrefabReplacer : MonoBehaviour
     public Transform newParent;
     public GameObject prefabReplacer;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-            Replace();
-    }
+    public bool randomizeRotation;
+    public bool randomizeSize;
+    public float sizeMultiplierMin;
+    public float sizeMultiplierMax;
 
-    void Replace()
+    public bool disableInsteadOfDelete;
+
+    public void Replace()
     {
         for (int i = 0; i < parentOfReplacees.childCount; i++)
         {
             Transform temp = parentOfReplacees.GetChild(i);
             GameObject newObj = Instantiate(prefabReplacer, temp.position, temp.rotation, newParent);
-            newObj.transform.localScale = temp.localScale;
+
+            if(randomizeSize)
+                newObj.transform.localScale = new Vector3(temp.localScale.x * Random.Range(sizeMultiplierMin, sizeMultiplierMax), temp.localScale.y * Random.Range(sizeMultiplierMin, sizeMultiplierMax), temp.localScale.z * Random.Range(sizeMultiplierMin, sizeMultiplierMax));
+            else
+                newObj.transform.localScale = temp.localScale;
+
+            if (randomizeRotation)
+                newObj.transform.localRotation = Quaternion.Euler(new Vector3(newObj.transform.localRotation.x, Random.Range(0, 360), newObj.transform.localRotation.z));
+
+            newObj.transform.position = temp.position;
         }
-        Destroy(parentOfReplacees.gameObject);
+        if (disableInsteadOfDelete)
+            parentOfReplacees.gameObject.SetActive(false);
+        else
+            DestroyImmediate(parentOfReplacees.gameObject);
     }
 }
