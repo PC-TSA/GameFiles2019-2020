06286ee73d51a6e7363674321b94f34fe874f3b5
Gabriel Moncau <gabriel.moncau@hotmail.com>
2020-03-05 20:13:42 -0500

    Hierarchy cleanup, started customization

---
+++ TSA Game 2019-2020/Assets/Scripts/ObjDragRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjDragRotate : MonoBehaviour
+{
+    public float rotSpeed = 20;
+    public bool x;
+    public bool y;
+
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        if(x)
+            transform.RotateAround(Vector3.up, -rotX);
+        if(y)
+            transform.RotateAround(Vector3.right, rotY);
+    }
+}
